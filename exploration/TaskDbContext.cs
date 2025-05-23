b3eb96df578ffe2bfdc8cb93f200d3c4aac95406
4lexzs <142885309+4lexzs@users.noreply.github.com>
2025-05-23 22:59:40 +0200

    Update TaskDbContext.cs

--- exploration/TaskDbContext.cs
+++ exploration/TaskDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using System;
 using TaskManager.Models;
 
 namespace TaskManager.Services
@@ -7,28 +6,34 @@ namespace TaskManager.Services
     public class TaskDbContext : DbContext
     {
         public DbSet<TaskItem> Tasks { get; set; }
-
-        // Konfiguriere die Datenbankverbindung
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // SQLite-Datenbankdatei im aktuellen Verzeichnis
             optionsBuilder.UseSqlite("Data Source=tasks.db");
         }
-
-        // Konfiguriere das Datenmodell
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TaskItem>()
                 .HasKey(t => t.Id);
-
+                
             modelBuilder.Entity<TaskItem>()
                 .Property(t => t.Title)
                 .IsRequired()
                 .HasMaxLength(100);
-
+                
             modelBuilder.Entity<TaskItem>()
                 .Property(t => t.Description)
                 .HasMaxLength(500);
+                
+            modelBuilder.Entity<TaskItem>()
+                .Property(t => t.Category)
+                .HasMaxLength(50)
+                .HasDefaultValue("Allgemein");
+                
+            modelBuilder.Entity<TaskItem>()
+                .Property(t => t.Priority)
+                .HasDefaultValue(1);
         }
     }
 }
