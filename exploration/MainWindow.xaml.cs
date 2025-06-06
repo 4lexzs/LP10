cef0167ec7bac49b5a6c143fe696c62719d28363
4lexzs <142885309+4lexzs@users.noreply.github.com>
2025-06-06 17:40:56 +0200

    Update MainWindow.xaml.cs

--- exploration/MainWindow.xaml.cs
+++ exploration/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using TaskManager.Models;
 using TaskManager.ViewModels;
 
@@ -8,37 +9,47 @@ namespace TaskManager
     public partial class MainWindow : Window
     {
         private MainViewModel _viewModel;
-        
+
         public MainWindow()
         {
             InitializeComponent();
-            
+
             _viewModel = new MainViewModel();
             DataContext = _viewModel;
         }
-        
+
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             _viewModel.AddTask();
         }
-        
+
+        private void TitleTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Enter-Taste zum Hinzufügen von Aufgaben
+            if (e.Key == Key.Enter)
+            {
+                _viewModel.AddTask();
+                e.Handled = true;
+            }
+        }
+
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
             if (sender is FrameworkElement element && element.Tag is TaskItem task)
             {
                 var result = MessageBox.Show(
-                    $"Möchtest du die Aufgabe '{task.Title}' wirklich löschen?", 
-                    "Aufgabe löschen", 
-                    MessageBoxButton.YesNo, 
+                    $"Möchtest du die Aufgabe '{task.Title}' wirklich löschen?",
+                    "Aufgabe löschen",
+                    MessageBoxButton.YesNo,
                     MessageBoxImage.Question);
-                
+
                 if (result == MessageBoxResult.Yes)
                 {
                     _viewModel.DeleteTask(task);
                 }
             }
         }
-        
+
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             if (sender is FrameworkElement element && element.Tag is TaskItem task)
@@ -46,7 +57,7 @@ namespace TaskManager
                 _viewModel.ToggleTaskCompletion(task);
             }
         }
-        
+
         private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
             if (sender is FrameworkElement element && element.Tag is TaskItem task)
