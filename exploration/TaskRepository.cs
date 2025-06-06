9ad2a3de98779c540e545e5e99b5b0fbf4efc4a0
4lexzs <142885309+4lexzs@users.noreply.github.com>
2025-06-06 17:39:44 +0200

    Update TaskRepository.cs

--- exploration/TaskRepository.cs
+++ exploration/TaskRepository.cs
@@ -6,43 +6,38 @@ namespace TaskManager.Services
 {
     public class TaskRepository : ITaskRepository
     {
-        private readonly TaskDbContext _context;
-
-        public TaskRepository()
-        {
-            _context = new TaskDbContext();
-            _context.Database.EnsureCreated();
-        }
-
         public List<TaskItem> GetAllTasks()
         {
-            return _context.Tasks.ToList();
-        }
-
-        public TaskItem GetTaskById(int id)
-        {
-            return _context.Tasks.Find(id);
+            using var context = new TaskDbContext();
+            context.Database.EnsureCreated();
+            return context.Tasks.ToList();
         }
 
         public void AddTask(TaskItem task)
         {
-            _context.Tasks.Add(task);
-            _context.SaveChanges();
+            using var context = new TaskDbContext();
+            context.Database.EnsureCreated();
+            context.Tasks.Add(task);
+            context.SaveChanges();
         }
 
         public void UpdateTask(TaskItem task)
         {
-            _context.Tasks.Update(task);
-            _context.SaveChanges();
+            using var context = new TaskDbContext();
+            context.Database.EnsureCreated();
+            context.Tasks.Update(task);
+            context.SaveChanges();
         }
 
         public void DeleteTask(int id)
         {
-            var task = _context.Tasks.Find(id);
+            using var context = new TaskDbContext();
+            context.Database.EnsureCreated();
+            var task = context.Tasks.Find(id);
             if (task != null)
             {
-                _context.Tasks.Remove(task);
-                _context.SaveChanges();
+                context.Tasks.Remove(task);
+                context.SaveChanges();
             }
         }
     }
