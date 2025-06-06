2e2df0181680b4af62bc75427afbefa31548b44e
4lexzs <142885309+4lexzs@users.noreply.github.com>
2025-06-06 17:38:55 +0200

    Update ITaskRepository.cs

--- exploration/ITaskRepository.cs
+++ exploration/ITaskRepository.cs
@@ -6,7 +6,6 @@ namespace TaskManager.Services
     public interface ITaskRepository
     {
         List<TaskItem> GetAllTasks();
-        TaskItem GetTaskById(int id);
         void AddTask(TaskItem task);
         void UpdateTask(TaskItem task);
         void DeleteTask(int id);
